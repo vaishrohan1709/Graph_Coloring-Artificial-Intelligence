75e8c1a2afaa5394a1de96e8804ac7b666b8970f
Rohan Vaish <vaishrohan@Rohans-MacBook-Pro.local>
2018-03-12 20:31:04 -0400

    DFSB basic complete

--- dfsb.py
+++ dfsb.py
@@ -9,7 +9,7 @@ from heapq import heappush, heappop
 class DFSB:
     def __init__(self,graph):
         self.graph=graph
-        self.states=list(self.graphs.keys())
+        self.states=list(self.graph.keys())
         self.assignment=None
         self.time=0
         self.over_time=False
@@ -18,33 +18,75 @@ class DFSB:
     def dfsb_solution(self):
         self.starting_time=time.time()
         self.assignment=self.recursive_dfsb({})
-        return self.solution
+        return self.assignment
 
     def recursive_dfsb(self,cur_assignment):
-        if self.num_assigned==len(list(self.graphs.keys()))
+        if self.num_assigned==len(list(self.graph.keys())):
             return cur_assignment
-
         self.time=time.time()- self.starting_time
-
         if self.time < 60:
             state= self.states[self.num_assigned]
             permitted_colors=self.graph[state]['Colors'][:]
-            for connected_state in self.graph[state]['Nodes']:
-                if cur_assignment[connected_state] in permitted_colors:
-                    permitted_colors.remove(ur_assignment[connected_state])
-
             for color in permitted_colors:
-
-
+                if self.isSafe(cur_assignment,state,color):
+                    temp_cur_assignment={**cur_assignment, **{state:color}}
+                    self.num_assigned=self.num_assigned+1
+                    temp_assignment=self.recursive_dfsb(temp_cur_assignment)
+                    if temp_assignment:
+                        return temp_assignment
+                    if self.over_time:
+                        return None
+            return None
 
         else:
             self.over_time= True
             return None
 
+    def isSafe(self,cur_assignment,state,color):
+        for connected_state in self.graph[state]['Nodes']:
+            if connected_state in cur_assignment:
+                if color==cur_assignment[connected_state]:
+                    return False
+        return True
 
 
+class DFSBPlus:
 
+    def __init__(self,graph):
+        self.graph = graph
+        self.states = list(self.graph.keys())
+        self.assignment = None
+        self.time = 0
+        self.over_time = False
+        self.num_assigned = 0
 
+    def dfsb_plus_solution(self):
+        self.starting_time = time.time()
+        self.assignment = self.recursive_dfsb({})
+        return self.assignment
+
+    def recursive_dfsb_plus(self,cur_assignment):
+        if self.num_assigned==len(list(self.graph.keys())):
+            return cur_assignment
+        self.time = time.time() - self.starting_time
+        
+        if self.time < 60:
+            state = self.states[self.num_assigned]
+            permitted_colors = self.graph[state]['Colors'][:]
+            for color in permitted_colors:
+                if self.isSafe(cur_assignment, state, color):
+                    temp_cur_assignment = {**cur_assignment, **{state: color}}
+                    self.num_assigned = self.num_assigned + 1
+                    temp_assignment = self.recursive_dfsb(temp_cur_assignment)
+                    if temp_assignment:
+                        return temp_assignment
+                    if self.over_time:
+                        return None
+            return None
+
+        else:
+            self.over_time = True
+            return None
 
 
 
@@ -68,28 +110,23 @@ def input_parse(input_file):
 
 
 if __name__ == '__main__':
+    mode=0
     if (str(sys.argv[3]) == '1'):
-        mode = 1
+        mode = 0
         print("DFSB")
     elif (str(sys.argv[3]) == '2'):
-        mode=2
+        mode=1
         print("DFSB++")
     input_file = str(sys.argv[1])
     output_file = str(sys.argv[2])
     graph=input_parse(input_file)
-    print(graph)
 
-    '''print('Output:')
-    if (algorithm == 1):
-        initialize = Astar(start_state, heuristic, type)
-        solution = initialize.A_star_solution()
-        print(solution)
+    if mode==0:
+        dfsb=DFSB(graph)
+        answer=dfsb.dfsb_solution()
+        print(answer)
+    if mode==1:
 
-    elif (algorithm == 2):
-        initialize = IDAstar(start_state, heuristic, type)
-        solution = initialize.IDAstar_solution()
-        print(solution)
 
-    output = open(output_file, "w")
-    output.write(solution)
-    output.close()'''
+
+
