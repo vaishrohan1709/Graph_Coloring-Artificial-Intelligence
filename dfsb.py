ac012685dadef9c78eb3a24ad34e1f114c709faa
Rohan Vaish <vaishrohan@Rohans-MacBook-Pro.local>
2018-03-14 12:30:59 -0400

    dfsb complete

--- dfsb.py
+++ dfsb.py
@@ -6,6 +6,7 @@ from collections import OrderedDict
 from operator import itemgetter
 from itertools import *
 from heapq import heappush, heappop
+from copy import deepcopy
 
 
 class DFSB:
@@ -15,25 +16,23 @@ class DFSB:
         self.assignment=None
         self.time=0
         self.over_time=False
-        self.num_assigned=0
 
     def dfsb_solution(self):
         self.starting_time=time.time()
-        self.assignment=self.recursive_dfsb({})
+        self.assignment=self.recursive_dfsb(0,{})
         return self.assignment
 
-    def recursive_dfsb(self,cur_assignment):
-        if self.num_assigned==len(list(self.graph.keys())):
+    def recursive_dfsb(self,var_assigned,cur_assignment):
+        if var_assigned==len(list(self.graph.keys())):
             return cur_assignment
         self.time=time.time()- self.starting_time
         if self.time < 60:
-            state= self.states[self.num_assigned]
-            permitted_colors=self.graph[state]['Colors'][:]
+            state= self.states[var_assigned]
+            permitted_colors=self.graph[state]['Colors']
             for color in permitted_colors:
                 if self.isSafe(cur_assignment,state,color):
                     temp_cur_assignment={**cur_assignment, **{state:color}}
-                    self.num_assigned=self.num_assigned+1
-                    temp_assignment=self.recursive_dfsb(temp_cur_assignment)
+                    temp_assignment=self.recursive_dfsb(var_assigned+1,temp_cur_assignment)
                     if temp_assignment:
                         return temp_assignment
                     if self.over_time:
@@ -60,75 +59,87 @@ class DFSBPlus:
         self.assignment = None
         self.time = 0
         self.over_time = False
-        self.num_assigned = 0
 
     def dfsb_plus_solution(self):
         self.starting_time = time.time()
-        self.assignment = self.recursive_dfsb_plus({})
+        self.assignment = self.recursive_dfsb_plus(0,{})
         return self.assignment
 
-    def recursive_dfsb_plus(self,cur_assignment):
-        if self.num_assigned==len(list(self.graph.keys())):
+    def recursive_dfsb_plus(self,var_assigned,cur_assignment):
+        if var_assigned==len(list(self.graph.keys())):
             return cur_assignment
         self.time = time.time() - self.starting_time
 
-        state=self.most_constrained_variable(cur_assignment)
-        print (state)
+        if self.time < 60:
+            state=self.most_constrained_variable(cur_assignment)
+            print ('current state', state)
 
-        lcv_colors_seq= self.least_constraining_value(state,cur_assignment)
-        print (lcv_colors_seq)
+            lcv_colors_seq= self.least_constraining_value(state,cur_assignment)
+            print ('color sequence', lcv_colors_seq)
 
-        '''if self.time < 60:
-            state = self.states[self.num_assigned]
-            permitted_colors = self.graph[state]['Colors'][:]
-            for color in permitted_colors:
-                if self.isSafe(cur_assignment, state, color):
-                    temp_cur_assignment = {**cur_assignment, **{state: color}}
-                    self.num_assigned = self.num_assigned + 1
-                    temp_assignment = self.recursive_dfsb(temp_cur_assignment)
+            for color in lcv_colors_seq:
+                print ('Checking color: ', color,' for: ', state)
+                backup= deepcopy(self.graph) #to remove
+                self.graph[state]['Colors']=[color]
+                if self.arc_consistency(cur_assignment,state):
+                    print ('Consistent')
+                    temp_assignment = self.recursive_dfsb_plus(var_assigned+1,{**cur_assignment, **{state: color}})
                     if temp_assignment:
+                        print ('current assignment', temp_assignment)
+                        print ("yoo\n")
                         return temp_assignment
-                    if self.over_time:
-                        return None
+                self.graph=backup
+
+                if self.over_time:
+                    print ('overtime None')
+                    return None
+
+            print ('Hey None')
             return None
 
         else:
-            self.over_time = True
-            return None'''
+            print ('overtime None')
+            self.over_time= True
+            return None
+
     def get_arcs(self,cur_assignment):
         assigned_states = cur_assignment.keys()
-        #arcs={}
         arcs=[]
         for state, value in self.graph.items():
             for head in value['Nodes']:
-                arcs.append((state,head))
-                #arcs[head].append((state,head))
+                if state not in assigned_states:
+                        arcs.append((state,head))
         return arcs
 
 
-    def arc_consistency(self, cur_assignment):
-        assigned_states = cur_assignment.keys()
+    def arc_consistency(self, cur_assignment,state):
+        assigned_states = list(cur_assignment.keys())
+        assigned_states.append(state)
         arcs=self.get_arcs(cur_assignment)
-        removed_colors = []
         while arcs:
             tail, head=arcs.pop(0)
             colors_h= self.graph[head]['Colors']
             colors_t=self.graph[tail]['Colors']
+            colors_t_copy=colors_t
             flag=0
             for color in colors_t:
-                if len(colors_h- {color})<0:
-                    self.graph[tail]['Colors'].remove(color)
-                    removed_colors.append(color)
-                    flag=1
-                if len(colors_t)==0:
-                    return False
-            if flag==1:
-                arcs.append(self.get_tail_arcs(arcs,tail))
+                if color in colors_h:
+                    if len(colors_h)==1:
+                        colors_t.remove(color)
+            if len(colors_t)!=0:
+                if colors_t_copy!=colors_t:
+                    print ('append')
+                    self.graph[tail]['Colors']=[colors_t]
+                    #arcs = list(set(arcs) | set(self.get_tail_arcs(arcs, tail)))
+                    arcs.append(self.get_tail_arcs(arcs,tail))
+            else:
+                print ('length gone')
+                return False
         return True
 
     def get_tail_arcs(self,arcs,tail):
         temp_arcs=[]
-        for t,h in arcs:
+        for (t,h) in arcs:
             if h==tail:
                 temp_arcs.append((t,h))
         return temp_arcs
@@ -137,7 +148,7 @@ class DFSBPlus:
 
 
 
-    def forward_check(self,state,color,cur_assignment):
+    '''def forward_check(self,state,color,cur_assignment):
         assigned_states = cur_assignment.keys()
         unassigned_states = []
         for state in self.graph.keys():
@@ -150,7 +161,7 @@ class DFSBPlus:
                     self.graph[neighbor]['Colors'].remove(color)
                     if len(self.graph[neighbor]['Colors'])==0:
                         return False
-        return True
+        return True'''
 
     def least_constraining_value(self,state,cur_assignment):
         colors=self.graph[state]['Colors'][:]
@@ -180,7 +191,7 @@ class DFSBPlus:
         for state in self.graph.keys():
             if state not in assigned_states:
                 unassigned_states.append(state)
-        print (unassigned_states)
+        #print (unassigned_states)
         mcv=sys.maxsize
         mcv_state=None
         for state in self.graph.keys():
@@ -210,7 +221,15 @@ def input_parse(input_file):
 
 
 
-
+def verify_solution(solution, graph):
+    if solution == None:
+        return True
+    for node in solution:
+        for dependent_node in graph[node]['Nodes']:
+            print ('in')
+            if solution[node] == solution[dependent_node]:
+                return False
+    return True
 
 if __name__ == '__main__':
     mode=0
@@ -229,11 +248,14 @@ if __name__ == '__main__':
         answer=dfsb.dfsb_solution()
         print(answer)
     if mode==1:
-        print ('hey')
         dfsb = DFSBPlus(graph)
-        dfsb.dfsb_plus_solution()
+        answer=dfsb.dfsb_plus_solution()
+        print (answer)
         #print(answer)
 
-
+    if verify_solution(answer,graph):
+        print ('correct')
+    else:
+        print ('incorrect')
 
 
